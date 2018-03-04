c1d25b9408907314074dceb57ff87033355fff72
soumya-ranjan7 <soumya.cr17@gmail.com>
2018-03-04 15:25:49 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Soumya',
+    author_email='soumya.cr17@gmail.com',
+    description='HospitalManagementSystem'
+)
